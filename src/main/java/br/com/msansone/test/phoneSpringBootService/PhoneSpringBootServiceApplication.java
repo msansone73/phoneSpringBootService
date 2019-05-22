6d1313798678599a36fc3419022c5544833627f9
marcio sansone <msansone@gmail.com>
2019-05-22 15:43:04 +0100

    first commit.

---
+++ src/main/java/br/com/msansone/test/phoneSpringBootService/PhoneSpringBootServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.msansone.test.phoneSpringBootService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneSpringBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneSpringBootServiceApplication.class, args);
+	}
+
+}
